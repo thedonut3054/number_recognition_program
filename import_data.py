1b4396cc064e2ed04f5fa971d3090d6c6237f557
thedonut3054 <136283010+thedonut3054@users.noreply.github.com>
2026-03-30 22:25:10 -0400

    I CAN SEE AN IMAGE

--- import_data.py
+++ import_data.py
@@ -1,1 +1,17 @@
+import torch
+import pandas as pd
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+import matplotlib.pyplot as plt
 
+training_data = datasets.QMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor(),
+)
+
+image_tensor, label_tensor = training_data[0]
+plt.imshow(image_tensor.numpy()[0],cmap='gray')
+plt.savefig("test")
