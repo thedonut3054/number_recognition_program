a0e59167f0023a425cffa5faca5433d890a14ecf
thedonut3054 <136283010+thedonut3054@users.noreply.github.com>
2026-05-06 20:22:03 +0000

    n/a

--- dataset_loading/import_EMNIST.py
+++ dataset_loading/import_EMNIST.py
@@ -2,7 +2,6 @@ import torch
 from torch.utils.data import DataLoader
 from torchvision import datasets
 import torchvision.transforms.v2 as v2
-import matplotlib.pyplot as plt
 import random
 from dataset_loading.mappings import char_map
 print("Importing data")
@@ -37,7 +36,3 @@ test_data = datasets.EMNIST(
 test_dataloader = DataLoader(test_data, batch_size=20, shuffle=True)
 
 print(len(test_data))
-image, label = training_data[random.randint(0, len(test_data))]
-print(chr(char_map[str(label)]))
-plt.imshow(image.permute(1, 2, 0).detach().cpu(), cmap='gray')
-plt.savefig('EMNIST_image.png')
